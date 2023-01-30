8d4f85f8790a5c78f8648b5f0a7c26dc86134cbc
karabinerdima2022 <artistofhisspace@gmail.com>
2023-01-30 08:43:58 +0300

    class Calculator added

--- homework.py
+++ homework.py
@@ -10,3 +10,27 @@ class Record:
             self.date = dt.date.today()
         else:
             self.date = dt.datetime.strptime(date, '%d.%m.%Y').date()
+
+
+class Calculator:
+    def __init__(self, limit):
+        self.limit = limit
+        self.records = []
+
+    def add_record(self, record):
+        self.records.append(record)
+
+    def get_stats(self, days):
+        today = dt.date.today()
+        first_day = today - dt.timedelta(days=days)
+
+        total_amount = sum(record.amount for record in self.records
+                           if first_day <= record.date <= today)
+
+        return total_amount
+
+    def get_today_stats(self):
+        return self.get_stats(days=0)
+
+    def get_week_stats(self):
+        return self.get_stats(days=7)
