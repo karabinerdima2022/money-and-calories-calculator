8671baa9ea721add1cd85acee641afab94147a72
karabinerdima2022 <artistofhisspace@gmail.com>
2023-01-30 16:08:58 +0300

    class CashCalculator added

--- homework.py
+++ homework.py
@@ -34,3 +34,32 @@ class Calculator:
 
     def get_week_stats(self):
         return self.get_stats(days=7)
+
+
+class CashCalculator(Calculator):
+    RUB_RATE = 1.0
+    USD_RATE = 69.75
+    EURO_RATE = 75.81
+
+    def get_today_cash_remained(self, currency='rub'):
+        currencies = {
+            'rub': ('руб', self.RUB_RATE),
+            'usd': ('USD', self.USD_RATE),
+            'eur': ('Euro', self.EURO_RATE)
+        }
+
+        if currency not in currencies:
+            return 'Данная валюта не поддерживается'
+
+        units, rate = currencies.get(currency)
+        balance = self.limit - self.get_today_stats()
+        converted_balance = balance / rate
+        rounded_balance = round(converted_balance, 2)
+
+        if balance > 0:
+            return f'На сегодня осталось {rounded_balance} {units}'
+        elif balance == 0:
+            return 'Денег нет, держись'
+        else:
+            return ('Денег нет, держись: твой долг -'
+                    f' {abs(rounded_balance)} {units}')
